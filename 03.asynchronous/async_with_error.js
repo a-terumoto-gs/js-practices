2906bdfb6667ebd304ef1cca7ee501a4b8688cb5
a-terumoto-gs <a.terumoto@genbasupport.com>
2023-12-15 18:47:35 +0900

    改行調整

--- 03.asynchronous/async_with_error.js
+++ 03.asynchronous/async_with_error.js
@@ -4,26 +4,21 @@ import { run_query, get_all, close_db } from "./function_preference.js";
 const db = new sqlite3.Database(':memory:');
 
 async function main() {
-  await run_query(
-    db,
-    "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-  );
-  try {
-    await run_query(db, "INSERT INTO book(title) VALUES(?)", ["SampleBook"]);
+  await run_query( db, "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",);
+  try { await run_query(db, "INSERT INTO book(title) VALUES(?)", ["SampleBook"]);
   } catch (e) {
     if (e instanceof Error) {
       console.log(e.message);
     }
   }
-  try {
-    await get_all(db, "SELECT books_id FROM books");
+  try { await get_all(db, "SELECT books_id FROM books");
   } catch (e) {
     if (e instanceof Error) {
       console.log(e.message);
     }
   }
   await run_query(db, "DROP TABLE books");
-  await close_db(db);
+  close_db(db);
 }
 
 main();
--- 03.asynchronous/callback_with_error.js
+++ 03.asynchronous/callback_with_error.js
@@ -5,15 +5,12 @@ const db = new sqlite3.Database(":memory:");
 function main() {
   db.run(
     "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-    () => {
-      db.run("INSERT INTO book(title) VALUES(?)", ["SampleBook"], (err) => {
-        if (err) {
-          console.error(err.message);
-        }
-        db.all("SELECT books_id FROM books", (err) => {
-          if (err) {
-            console.error(err.message);
-          }
+    () => { db.run("INSERT INTO book(title) VALUES(?)", ["SampleBook"],
+      function (err) {
+        if (err) { console.error(err.message); }
+          db.all("SELECT books_id FROM books", 
+            (err) => {
+              if (err) { console.error(err.message); }
           db.run("DROP TABLE books");
           db.close();
         });
