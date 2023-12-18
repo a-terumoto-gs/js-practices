d7f5003a23bae8906095bbe86d1230acc243ece7
a-terumoto-gs <a.terumoto@genbasupport.com>
2023-12-18 14:41:32 +0900

    Promise追加

---
+++ 03.asynchronous/promise_with_error.js
@@ -0,0 +1,32 @@
+import sqlite3 from "sqlite3";
+import { run_query, get_all, close_db } from "./function_preference.js";
+
+const db = new sqlite3.Database(":memory:");
+
+function main() {
+  run_query(
+    db,
+    "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+  )
+    .then(() =>
+      run_query(db, "INSERT INTO reports(title) VALUES(?)", ["SampleBook"]),
+    )
+    .catch((err) => {
+      console.error(err.message);
+    })
+
+    .then(() => get_all(db, "SELECT report_id FROM books"))
+    .catch((err) => {
+      console.error(err.message);
+    })
+
+    .then(() => {
+      run_query(db, "DROP TABLE books");
+    })
+
+    .then(() => {
+      close_db(db);
+    });
+}
+
+main();
