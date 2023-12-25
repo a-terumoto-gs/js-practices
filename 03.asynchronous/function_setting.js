541026cc2ca9d3531387905c2b7939a8aecefb54
a-terumoto-gs <a.terumoto@genbasupport.com>
2023-12-25 13:49:24 +0900

    ファイル名変更

---
+++ 03.asynchronous/function_setting.js
@@ -0,0 +1,35 @@
+export function run_query(db, query, params = []) {
+  return new Promise((resolve, reject) => {
+    db.run(query, params, function (err) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve({ lastID: this.lastID });
+      }
+    });
+  });
+}
+
+export function get_all(db, query, params = []) {
+  return new Promise((resolve, reject) => {
+    db.all(query, params, function (err, book) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(book);
+      }
+    });
+  });
+}
+
+export function close_db(db) {
+  return new Promise((resolve, reject) => {
+    db.close(function (err) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
